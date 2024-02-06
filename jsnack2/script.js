9fffc7c65d7fb0ba3544d8a903f4e573590570bf
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-06 11:26:08 +0100

    exercise 2 loaded and fonts folder corrected

---
+++ jsnack2/script.js
@@ -0,0 +1,20 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+let founded = false;
+let oddNumber;
+let resultElement = document.querySelector("#result");
+
+do {
+    oddNumber = Number(prompt("Inserisci numero"));
+    if (oddNumber > randomNumber) {
+        alert("Numero troppo alto");
+    }
+    else if (oddNumber < randomNumber) {
+        alert("Numero troppo basso");
+    } else {
+        alert("Hai indovinato");
+        founded = true;
+    }
+} while (founded !== true);
+
+resultElement.innerHTML = `Il numero individuato e': ${randomNumber}`
